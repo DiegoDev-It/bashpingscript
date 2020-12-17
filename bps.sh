89d15e327098da00c87e315cd3f1379acad5a243
DiegoDev-It <72506891+DiegoDev-It@users.noreply.github.com>
2020-12-17 09:42:57 +0100

    Update bps.sh
    Mistake

--- bps.sh
+++ bps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Script of server check.
 echo ------------------------------------------------------------------------------------------------------------ >> /path/to/output.txt
-date /path/to/output.txt
+date >> /path/to/output.txt
 ping -c [seconds] [ip] >> /path/to/output.txt  #Change values in [ ]!
 echo ------------------------------------------------------------------------------------------------------------ >> /path/to/output.txt
