ec387b885910fe2d87f3a34f2cd26981beec7618
Diego Gaglione <72506891+DiegoDev-It@users.noreply.github.com>
2020-12-19 15:06:26 +0100

    Update bps.sh
    Added new code for extend functions. For example, adding the user which runs the script and an auto-kill of the script at ending of its job.

--- bps.sh
+++ bps.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo Script of server check.
+echo "Bash Ping Script. Please support me visiting my GitHub page https://github.com/DiegoDev-It/. Feel free to contact about bugs or 
+requests at diwgo3sk@protonmail.com
+Your user is:" 
+whoami >> /path/to/output.txt #User that runs script
+echo "Running..."
 echo ------------------------------------------------------------------------------------------------------------ >> /path/to/output.txt
 date >> /path/to/output.txt
-ping -c [seconds] [ip] >> /path/to/output.txt  #Change values in [ ]!
+ping -c [seconds] [ip] >> /path/to/output.txt  #Change values in [ ]! #Ping command.
 echo ------------------------------------------------------------------------------------------------------------ >> /path/to/output.txt
+echo "Finished!"
+kill $$
